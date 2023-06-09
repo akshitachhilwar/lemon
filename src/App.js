7a419f4d17d1cba8530f1729d43807c3f6683f03
akshita <akshitachhilwar99@gmail.com>
2023-06-09 14:20:42 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <React.Fragment className="App">
+    <header>
+      <h1> Welcome</h1>
+
+    </header>
+
+    <nav>
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+          </ul>
+        </nav>
+
+        <footer>
+        <p>Contact</p>
+        <p>Links To Social Media</p>
+      </footer>
+
+
+
+    </React.Fragment>
+
   );
 }
 
